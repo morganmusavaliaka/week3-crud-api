5d7ed678db35c027873ba0522a6e3a989460f451
morganmusavaliaka <morganmusava@gmail.com>
2026-08-18 03:26:20 +0300

    Add GET/Todos/:id, POST validation, and GET/Todos/active

---
+++ app.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+// parsing middleware
+app.use(express.json());
+
+
+
+let todos = [
+  { id: 1, task: 'Learn Node.js', completed: false },
+  { id: 2, task: 'Build CRUD API', completed: false },
+];
+
+app.get('/todos', (req, res) => {
+  res.status(200).json(todos); // Send array as JSON
+});
+
+app.get('/todos/active', (req, res) => {
+  const activeTodos = todos.filter(t => !t.completed);
+  res.status(200).json(activeTodos);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id));
+  if(!todo) return res.status(404).json({message: "Todo not found"});
+  res.status(200).json(todo);
+});
+
+
+
+
+app.post('/todos', (req, res) => { 
+  if(!req.body.task) {
+    return res.status(400).json({message:"Task field is required"});
+  }
+  const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
+  todos.push(newTodo);
+  res.status(201).json(newTodo); // Echo back
+});
+
+// const createTodo = (req, res) => {
+//   const newTodo = { id: todos.length + 1, ...req.body }; 
+//   todos.push(newTodo);
+//   res.status(201).json(newTodo);
+// };
+
+// app.post('/todos', createTodo);
+
+// PATCH Update – Partial
+app.patch("/todos/:id", (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id)); // Array.find()
+  if (!todo) return res.status(404).json({ message: "Todo not found" });
+  Object.assign(todo, req.body); // Merge: e.g., {completed: true}
+  res.status(200).json(todo);
+});
+
+
+// DELETE Remove
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const initialLength = todos.length;
+  todos = todos.filter(t => t.id !== id); // Array.filter() - non-destructive
+  if (todos.length === initialLength) return res.status(404).json({ error: "Not found" });
+  res.status(204).send(); // Silent success
+});
+
+app.use((err,req,res,next) => {
+  res.status(500).json({error:'server error'});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`)
+});
